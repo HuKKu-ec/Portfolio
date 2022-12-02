563279171a737b8e7c25693451990e37354dfd2a
unknown <hukbathelachola2311@gmail.com>
2022-12-02 21:10:58 +0530

    margin top is added button in exp  page

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -23,14 +23,14 @@ const Experience = () => {
         <Container>
           <Row>
             <Col xs={12} sm={6}>
-              <div className="box">
+              <div className="box mt-2">
                 {' '}
                 As part of IEDC SS College I made my contribution in UI design
                 of the Website for a Canada based Foundation named Irfaa
               </div>
             </Col>
             <Col xs={12} sm={6}>
-              <div className="box">
+              <div className="box mt-2">
                 As part of my college project I made an app named Kaarunya for
                 palliative care units by Using React Native and Firebase.({' '}
                 <a href="https://expo.dev/artifacts/d8d0591d-c5b5-41a8-a794-83fe8dfe42a2">
@@ -41,7 +41,7 @@ const Experience = () => {
               </div>
             </Col>
             <Col xs={12} sm={6}>
-              <div className="box">
+              <div className="box mt-3">
                 I got a chance to give training to students of Majlis arts and
                 science college about Robotics and IOT.
               </div>
